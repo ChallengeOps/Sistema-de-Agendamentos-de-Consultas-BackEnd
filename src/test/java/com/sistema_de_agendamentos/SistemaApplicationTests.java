223b8ea0eebd06e4bc74d47bea1b6d2b31add963
bootcamp04 <bootcamp04@deloitte.com>
2025-05-30 16:05:35 -0300

    iniciando Projeto

---
+++ src/test/java/com/sistema_de_agendamentos/SistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_de_agendamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
