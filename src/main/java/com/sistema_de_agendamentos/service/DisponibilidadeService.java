3630c83194d76821b26654874e4b0cc4aa5f2756
Caua emanuel <emanuellcaua8@gmail.com>
2025-06-05 10:26:05 -0300

    versao final

--- src/main/java/com/sistema_de_agendamentos/service/DisponibilidadeService.java
+++ src/main/java/com/sistema_de_agendamentos/service/DisponibilidadeService.java
@@ -51,6 +51,7 @@ public class DisponibilidadeService {
 
         var disponibilidade = new Disponibilidade();
         disponibilidade.setProfissional(usuario);
+
         disponibilidade.setHoraInicio(inicio);
         disponibilidade.setHoraFim(fim);
 
